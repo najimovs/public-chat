67593b3a771e9513eee51c26b07301f731cb2f09
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-03-17 15:53:00 +0500

    setup socket server

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,27 @@
-console.log( "API" )
+import { createServer } from "node:http"
+import { Server } from "socket.io"
+
+const PORT = 3000
+
+const httpServer = createServer( ( req, res ) => {
+
+	res.writeHead( 200, { "Content-Type": "text/plain" } )
+	res.end( "Socket server is running" )
+} )
+
+const io = new Server( httpServer, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ],
+	}
+} )
+
+io.on( "connection", browser => {
+
+	console.log( "New connection" )
+} )
+
+httpServer.listen( PORT, "0.0.0.0", () => {
+
+	console.log( `Server listening on port ${ PORT }` )
+} )
